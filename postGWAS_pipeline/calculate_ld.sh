3fda1de153bcd92319af1f90ecd23e35cd9f2a32
stevenpur <stevenpur@hotmail.com>
2023-12-28 05:24:16 +0000

    2

--- postGWAS_pipeline/calculate_ld.sh
+++ postGWAS_pipeline/calculate_ld.sh
@@ -4,7 +4,8 @@
 # set the parameters
 in_dir=$HOME/bugbank_data/postgwas_regenie/sig_snp
 out_dir=$HOME/bugbank_data/postgwas_regenie/sig_snp_ld
-bgen_dir=/well/ukbb-wtchg/v3/imputation/
+bgen_dir=/well/ukbb-wtchg/v3/imputation
+samp_dir=/well/bag/wilson/ukb
 snpid_col=3
 # temporary output file for pooling all filtered snps together
 pool_snp_prefix=$out_dir/tmp_poolsnps_lst
@@ -35,3 +36,67 @@ do
         tail -n +2 |
         awk -v pool_snp_prefix=${pool_snp_prefix} -v snpid_col=${snpid_col} '{print $0 >> pool_snp_prefix".chr"$1".lst"}'
 done
+
+
+# loop through the autosomes and calculate LD
+for chrs in {1..22}
+do
+    # check if the file exist, create empty file and skip this chromosome
+    if [ ! -f $pool_snp_prefix.chr${chrs}.lst ]
+    then
+        touch $pool_snp_prefix.chr${chrs}.lst
+        continue
+    fi
+    # get the corresponding genotype file
+    bgen_f=$bgen_dir/ukb_imp_chr${chrs}_v3.bgen
+    # to avoid running out of disk space, run at most 5 chromosomes at the same time
+    while [ $(ps | grep plink | wc -l) -gt 5 ]
+    do
+        echo "too many jobs running, sleeping for 300 seconds..."
+        sleep 5
+    done
+    # use plink to calculate the pairwise LD, run in background for parallelization
+    (
+        plink2 --bgen $bgen_f ref-first\
+            --sample ${samp_dir}/ukb53100_imp_chr1_v3_s487296.sample \
+            --extract $pool_snp_prefix.chr${chrs}.lst \
+            --r2-phased \
+            --ld-window-kb 1000 \
+            --out $out_dir/ld.filtered_snps.chr${chrs}
+    )&
+done
+
+# chromosome X is a bit different
+# we need to extract the snps from both chrX and chrXY from the .bgen files
+bgen_x=$bgen_dir/ukb_imp_chrX_v3.bgen
+bgen_xy=$bgen_dir/ukb_imp_chrXY_v3.bgen
+# extract from bgen_x
+(
+    plink2 --bgen $bgen_x ref-first\
+        --sample ${samp_dir}/ukb53100_imp_chrX_v3_s486645.sample  \
+        --extract $pool_snp_prefix.chrX.lst \
+        --make-bed \
+        --out $out_dir/filtered_snps.chrX
+)
+# extract from bgen_xy
+(
+    plink2 --bgen $bgen_xy ref-first\
+        --sample ${samp_dir}/ukb53100_imp_chrXY_v3_s486331.sample \
+        --extract $pool_snp_prefix.chrX.lst \
+        --make-bed \
+        --out $out_dir/filtered_snps.chrXY
+)
+# merge the two files
+(
+    plink2 --bfile $out_dir/filtered_snps.chrX \
+        --bmerge $out_dir/filtered_snps.chrXY.bed $out_dir/filtered_snps.chrXY.bim $out_dir/filtered_snps.chrXY.fam \
+        --make-bed \
+        --out $out_dir/filtered_snps.chrX_merged
+)
+# calculate LD
+(
+    plink2 --bfile $out_dir/filtered_snps.chrX \
+        --r2-phased \
+        --ld-window-kb 1000 \
+        --out $out_dir/ld.filtered_snps.chrX
+)
